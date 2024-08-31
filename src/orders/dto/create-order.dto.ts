8a06a64dd35dd24b19f0d5dab146655c8d4cdd81
Erick Lasluisa <erickl1709@gmail.com>
2024-08-30 23:05:20 -0500

    chore: Update Prettier configuration and Docker Compose file
    The Prettier configuration file (.prettierrc) was updated to include the "endOfLine" option set to "crlf". This change ensures consistent line endings in the codebase.
    
    In the Docker Compose file (docker-compose.yml), the whitespace around the "POSTGRES_USER" and "POSTGRES_PASSWORD" environment variables was removed for consistency.
    
    No functional changes were made to the CreateOrderDto and OrderItemDto files.
    
    ```

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,40 +1,45 @@
-import { OrderStatus } from "@prisma/client"
-import { ArrayMinSize, IsArray, IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive, Validate, ValidateNested } from "class-validator"
-import { OrderStatusList } from "../enum/order.enum"
-import { Type } from "class-transformer"
-import { OrderItemDto } from "./order-item.dto"
+import { OrderStatus } from '@prisma/client';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  Validate,
+  ValidateNested,
+} from 'class-validator';
+import { OrderStatusList } from '../enum/order.enum';
+import { Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-    @IsNumber()
-    @IsPositive()
-    clientId: number
-    
-    @IsArray()
-    @ArrayMinSize(1)
-    @ValidateNested({ each: true })
-    @Type(() => OrderItemDto)
-    items: OrderItemDto[]
-    
-    
-    
-    
-    
-    // @IsNumber()
-    // @IsPositive()
-    // totalAmount: number
-    
-    // @IsNumber()
-    // @IsPositive()
-    // totalItems: number
+  @IsNumber()
+  @IsPositive()
+  clientId: number;
 
-    // @IsEnum(OrderStatusList,{
-    //     message: `Posible status values are ${OrderStatusList}`
-    // })
-    // @IsOptional()
-    // status: OrderStatus = OrderStatus.PENDING
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 
-    // @IsBoolean()
-    // @IsOptional()
-    // paid: boolean = false
+  // @IsNumber()
+  // @IsPositive()
+  // totalAmount: number
 
+  // @IsNumber()
+  // @IsPositive()
+  // totalItems: number
+
+  // @IsEnum(OrderStatusList,{
+  //     message: `Posible status values are ${OrderStatusList}`
+  // })
+  // @IsOptional()
+  // status: OrderStatus = OrderStatus.PENDING
+
+  // @IsBoolean()
+  // @IsOptional()
+  // paid: boolean = false
 }
